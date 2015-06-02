9cc80ad28f7dd91ad2e24d94c97237d20ca156d4
Viktor Gardart <viktor.gardart@agigen.se>
2015-06-02 22:52:17 +0200

    Google login kinda works

--- Ideatap/AppDelegate.swift
+++ Ideatap/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -39,6 +38,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        return GPPURLHandler.handleURL(url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
 
--- Ideatap/ViewController.swift
+++ Ideatap/ViewController.swift
@@ -8,27 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GPPSignInDelegate {
 
-    @IBOutlet weak var conditionLabel: UILabel!
-    @IBOutlet weak var conditionField: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var logoutBtn: UIButton!
+    @IBOutlet weak var username: UILabel!
     
-    var fb = Firebase(url:"https://idea-tap.firebaseio.com/condition")
+    var fb = Firebase(url: "https://idea-tap.firebaseio.com")
+    
+    func authenticateWithGoogle() {
+        var signIn = GPPSignIn.sharedInstance()
+        signIn.shouldFetchGooglePlusUser = true
+        signIn.clientID = "519366190805-r2hos4b0b303ke5dobqdor5cab03867l.apps.googleusercontent.com"
+        signIn.scopes = ["email"]
+        signIn.delegate = self
+        signIn.authenticate()
+    }
+    
+    func finishedWithAuth(auth: GTMOAuth2Authentication!, error: NSError!) {
+        if error == nil {
+            fb.authWithOAuthProvider("google", token: auth.accessToken,
+                withCompletionBlock: { error, authData in
+                    if error == nil {
+                        // User is now logged in!
+                        self.updateUI(true)
+                        
+                        let newUser = [
+                            "provider": authData.provider,
+                            "email": authData.providerData["email"] as! String //Should check if exists lol
+                        ]
+                        self.fb.childByAppendingPath("users")
+                            .childByAppendingPath(authData.uid).setValue(newUser)
+                    }
+            })
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        fb.observeEventType(.Value, withBlock: { snapshot in
-            if let condition = snapshot.value as? String {
-                self.conditionLabel.text = condition
-            }
-        })
+        if fb.authData != nil {
+            updateUI(true)
+        }
         
     }
     
-    @IBAction func updateCondition(sender: AnyObject) {
-        fb.setValue(conditionField.text)
+    @IBAction func logout(sender: AnyObject) {
+        fb.unauth()
+        updateUI(false)
+    }
+    @IBAction func login(sender: AnyObject) {
+        authenticateWithGoogle()
+    }
+    
+    func updateUI(isLoggedIn: Bool) {
+        if isLoggedIn == true {
+            loginBtn.hidden = true
+            username.hidden = false
+            logoutBtn.hidden = false
+            if let fullname = fb.authData.providerData["displayName"] as? String {
+                username.text = fullname
+            }
+        }else {
+            loginBtn.hidden = false
+            username.hidden = true
+            logoutBtn.hidden = true
+        }
     }
 
     override func didReceiveMemoryWarning() {
