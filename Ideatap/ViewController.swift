4acd2a246305f1f2799459c61672628d9ff7aa17
Viktor Gardart <viktor.gardart@agigen.se>
2015-06-02 20:57:23 +0200

    Firebase test

--- Ideatap/ViewController.swift
+++ Ideatap/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var conditionField: UITextField!
+    
+    var fb = Firebase(url:"https://idea-tap.firebaseio.com/condition")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        fb.observeEventType(.Value, withBlock: { snapshot in
+            if let condition = snapshot.value as? String {
+                self.conditionLabel.text = condition
+            }
+        })
+        
+    }
+    
+    @IBAction func updateCondition(sender: AnyObject) {
+        fb.setValue(conditionField.text)
     }
 
     override func didReceiveMemoryWarning() {
